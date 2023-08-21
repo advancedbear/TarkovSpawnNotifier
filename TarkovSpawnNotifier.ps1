1b7897ba2265bbf8442c066418b3de88f04eb1f1
advancedbear <advanced.bear@gmail.com>
2023-08-22 01:24:01 +0900

    Output more information

--- TarkovSpawnNotifier.ps1
+++ TarkovSpawnNotifier.ps1
@@ -6,7 +6,7 @@ $eft = "D:\Battlestate Games\eft\Logs"
 
 ##################################################
 
-Write-Host "Escape From Tarkov Spawn Notifier`r`n      © 2021 advanced_bear" -ForegroundColor Cyan
+Write-Host "Escape From Tarkov Spawn Notifier`r`n    © 2021-2023 advanced_bear" -ForegroundColor Cyan
 
 if(-not(Test-Path $eft)) {
     Write-Host "------------- ERROR -------------" -ForegroundColor White -BackgroundColor Red
@@ -26,6 +26,7 @@ $notify.visible = $true
 ## Define Target Log File (Latest "trace.log")
 $latest = (Get-ChildItem $eft | Sort-Object LastWriteTime -Descending)[0].FullName
 $target = (Get-ChildItem $latest\*traces*.log | Sort-Object LastWriteTime -Descending)[0].FullName
+
 ## Reading Tail line for Monitoring
 Write-Host "----------- Activated -----------" -ForegroundColor White -BackgroundColor DarkCyan
 Get-Content -Wait -Tail 10 -Path $target | ForEach-Object {
@@ -37,9 +38,19 @@ Get-Content -Wait -Tail 10 -Path $target | ForEach-Object {
         Write-Host "$now`tGame Starting!"
         $notify.showballoontip(10,'Tarkov Spawn Notifier','Game Starting!',[system.windows.forms.tooltipicon]::None)
     } elseif ($_ -like "*profileStatus*") {
-        $_ -match "(Location: .*?)," > $null
+        $_ -match "Location: (.*?)," > $null
         $Location = $Matches[1]
-        Write-Host "$now`t$Location"
+        Write-Host "$now`tLocation      : $($Location)"
+        $_ -match "Ip: (\d+\.\d+\.\d+\.\d+)" > $null
+        $ProgressPreference = "SilentlyContinue"
+        $TestResult = $(ping -w 500 -n 10 $Matches[1] | Select-Object -Last 4)
+        $WhoisResult = Invoke-RestMethod "http://rdap.db.ripe.net/ip/$($Matches[1])"
+        $Registrant = $WhoisResult.entities | Where-Object {$_.roles -match "registrant"}
+        Write-Host "$now`t-------Tarkov Server Information-------"
+        Write-Host "`t`t`tServer IPaddr : $($Matches[1])"
+        Write-Host "`t`t`tServer Country: $($WhoisResult.country)"
+        Write-Host "`t`t`tRatency (Ping): $($TestResult[3].Trim())"
+        Write-Host "`t`t`tPacket Loss   : $($TestResult[1].Trim())"
     } elseif ($_ -like "*ClientMetricsEvents*") {
         Write-Host "$now`tStart Match Making!"
     } elseif ($_ -like "*PlayerSpawnEvent*") {
